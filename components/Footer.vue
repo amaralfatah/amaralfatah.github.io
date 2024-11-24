bea2ccf8f12086f0adf0b3842ab7d828315c3083
amaralfatah <amaralfatah.main@gmail.com>
2024-11-24 17:42:30 +0700

    update

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,11 +2,8 @@
   <footer class="flex justify-center mx-auto">
     <span class="text-sm text-mygray">
       © {{ new Date().getFullYear() }}
-      <NuxtLink
-        class="text-mywhite hover:opacity-90"
-        to="https://yourcompany.com"
-      >
-        Must5
+      <NuxtLink class="text-mywhite hover:opacity-90" to="https://amar.my.id">
+        MustFive
       </NuxtLink>
     </span>
   </footer>
