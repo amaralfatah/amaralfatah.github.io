8488fa86e0d612771e3ae23ade633d60e8e1ef88
amaralfatah <amaralfatah.main@gmail.com>
2024-11-25 19:27:16 +0700

    try to deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,39 @@ export default defineNuxtConfig({
   dir: {
     assets: "assets",
   },
+  app: {
+    head: {
+      title: "Amar Al Fatah",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "Personal website of Amar Al Fatah",
+        },
+      ],
+      link: [
+        {
+          rel: "canonical",
+          href: "https://amar.my.id",
+        },
+      ],
+    },
+    baseURL: "/", // Karena menggunakan custom domain
+    buildAssetsDir: "assets",
+  },
+  // SSG Configuration
+  ssr: true,
+  nitro: {
+    preset: "github-pages",
+    routeRules: {
+      "/**": {
+        headers: {
+          "Cache-Control": "max-age=300, s-maxage=3600",
+          "X-Frame-Options": "DENY",
+        },
+      },
+    },
+  },
 });
---
+++ robots.config.ts
@@ -0,0 +1,6 @@
+export default [
+  { UserAgent: "*" },
+  { Allow: "/" },
+  { BlankLine: true },
+  { Sitemap: "https://amar.my.id/sitemap.xml" },
+];
